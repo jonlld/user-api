5dfa88736715c978e3210c88b99fd4d275722dc6
jonlld <87318928+jonlld@users.noreply.github.com>
2023-05-26 16:22:20 +0900

    feat: Add delete user endpoint with id match check

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,18 @@
 import express, { Request, Response } from "express";
 import { knex } from "../database/database";
-import { authenticateToken } from "../middleware/authenticateToken";
+import {
+  AuthenticatedRequest,
+  authenticateToken,
+} from "../middleware/authenticateToken";
 
 const router = express.Router();
+
+/*
+Notes: 
+- All routes use authenticateToken middleware to authorise prior to actioning
+- Note, the below assumes user is *not* admin
+- To this effect, using middleware to access current userId and check match for id-related requests 
+*/
 
 // Get all users
 router.get("/", authenticateToken, async (req: Request, res: Response) => {
@@ -39,6 +49,7 @@ router.get("/:id", authenticateToken, async (req: Request, res: Response) => {
   }
 });
 
+// Update specific user by id
 router.put("/:id", authenticateToken, async (req: Request, res: Response) => {
   try {
     // Get id and payload, renaming properties
@@ -56,6 +67,34 @@ router.put("/:id", authenticateToken, async (req: Request, res: Response) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+// Delete specific user by id
+router.delete(
+  "/:id",
+  authenticateToken,
+  async (req: AuthenticatedRequest, res: Response) => {
+    // Get user id
+    const { id } = req.params;
+
+    if (req.userId === id) {
+      try {
+        // Query db; returns num of deleted rows (0 or 1)
+        const deletedCount = await knex("users").where({ id }).del();
+
+        if (deletedCount > 0) {
+          res.status(200).json({ message: "User deleted successfully" });
+        } else {
+          // Send not found status and message
+          res.status(404).json({ message: "User not found" });
+        }
+      } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+      }
+    } else {
+      res.status(401).json({ error: "Unauthorized" });
+    }
+  }
+);
 
 // Named export, to differentiate
 export { router as userRoutes };
