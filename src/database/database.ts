5a19486db97264ceabc23c5116d477cbbde9e43f
jonlld <87318928+jonlld@users.noreply.github.com>
2024-03-28 11:00:41 +0000

    docs: Add notes to database.ts to clarify knex config values

--- src/database/database.ts
+++ src/database/database.ts
@@ -6,8 +6,9 @@ dotenv.config();
 
 // Dynamically set environment
 const environment = process.env.NODE_ENV || "development";
+// Will access production config or dev config, depending on environment
 const configOptions = knexConfig[environment];
-
+// Initialise with the above config
 const knexInstance = knex(configOptions);
-
+// Export for use in app
 export { knexInstance as knex };
